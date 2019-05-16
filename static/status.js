d3dbd5b8b11d913da89db81f87ef050ae944fb8e
keldu <gitea@keldu.de>
2019-05-16 12:55:06 +0200

    changed underlying structure

--- static/status.js
+++ static/status.js
@@ -1,18 +1,18 @@
-var services = {
+var modules = {
 	states:[],
 
-	service:function(info){
+	module:function(info){
 		this.name = info.name;
 		this.info = info.info;
 		this.file = info.file;
-		this.node = services.node("div", "service", "", services.node("img", "service-image"));
-		subnode = services.node("div", "service-text", "", services.node("div", "service-meta", "", services.node("h1", "", this.name)));
-		subnode.appendChild(services.node("div", "service-info"));
+		this.node = modules.node("div", "module", "", modules.node("img", "module-image"));
+		subnode = modules.node("div", "module-text", "", modules.node("div", "module-meta", "", modules.node("h1", "", this.name)));
+		subnode.appendChild(modules.node("div", "module-info"));
 		this.node.appendChild(subnode);
 
-		this.node.getElementsByClassName("service-meta")[0].appendChild(document.createTextNode(this.info));
-		this.node.getElementsByClassName("service-info")[0].textContent = " -- no data yet --";
-		this.node.getElementsByClassName("service-image")[0].src = "static/reported.svg";
+		this.node.getElementsByClassName("module-meta")[0].appendChild(document.createTextNode(this.info));
+		this.node.getElementsByClassName("module-info")[0].textContent = " -- no data yet --";
+		this.node.getElementsByClassName("module-image")[0].src = "static/reported.svg";
 
 		document.getElementById("center-box").appendChild(this.node);
 
@@ -20,27 +20,27 @@ var services = {
 			var _self = this;
 			ajax.asyncGet(this.file, function(request){
 				if(request.status != 200){
-					_self.node.getElementsByClassName("service-info")[0].textContent = "Failed to fetch";
+					_self.node.getElementsByClassName("module-info")[0].textContent = "Failed to fetch";
 					return;
 				}
 				try{
 					var data = JSON.parse(request.responseText);
 				}
 				catch(e){
-					_self.node.getElementsByClassName("service-info")[0].textContent = "Failed to parse incoming data";
+					_self.node.getElementsByClassName("module-info")[0].textContent = "Failed to parse incoming data";
 					return;
 				}
 				_self.state = data;
 				_self.update();
 			},
 			function(e){
-				this.node.getElementsByClassName("service-info")[0].textContent = "Failed to fetch";
+				this.node.getElementsByClassName("module-info")[0].textContent = "Failed to fetch";
 			});
 		};
 
 		this.update = function(){
-			this.node.getElementsByClassName("service-image")[0].src = "static/" + this.state.state + ".svg";
-			this.node.getElementsByClassName("service-info")[0].innerHTML = "Last check: " + this.state.last + "<br/>" + this.state.info;
+			this.node.getElementsByClassName("module-image")[0].src = "static/" + this.state.state + ".svg";
+			this.node.getElementsByClassName("module-info")[0].innerHTML = "Last check: " + this.state.last + "<br/>" + this.state.info;
 		};
 	},
 
@@ -71,21 +71,21 @@ var services = {
 				window.alert("Failed to parse info.json: " + e);
 				return;
 			}
-			data.forEach(function(service){
-				services.states.push(new services.service(service));
+			data.forEach(function(module){
+				modules.states.push(new modules.module(module));
 			});
-			services.update();
+			modules.update();
 		},
 		function(e){
 			window.alert("Failed to load status information: " + e);
 		});
 
-		window.setInterval(services.update, 10000);
+		window.setInterval(modules.update, 10000);
 	},
 
 	update:function(){
-		services.states.forEach(function(service){
-			service.fetch();
+		modules.states.forEach(function(module){
+			module.fetch();
 		});
 	}
 };
