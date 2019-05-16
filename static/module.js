020b6f4642d6ff068a5568903a0670ca541b2dc9
keldu <gitea@keldu.de>
2019-05-16 22:23:19 +0200

    bug fixing and minor behaviour improvement

--- static/module.js
+++ static/module.js
@@ -10,8 +10,9 @@ var modules = {
 		this.info = info.info;
 		this.modulename = info.module;
 		this.node = document.createElement("li");
+		this.file = "module/"+info.module+"/"+info.module+".json";
 		button = document.createElement("button");
-		button.onclick = function(){modules.tosite(info.module)};
+		button.onclick = function(){modules.tosite(info)};
 		subnode = document.createTextNode(this.name);
 		button.appendChild(subnode);
 		this.node.appendChild(button);
@@ -20,26 +21,53 @@ var modules = {
 	},
 
 	modulehost:function(info){
+		this.state = info;
+
 		this.node = modules.node("tr","content-element");
-		this.node.appendChild(modules.node("td","",info.status));
-		this.node.appendChild(modules.node("td","",info.name));
-		this.node.appendChild(modules.node("td","",info.address));
-		this.node.appendChild(modules.node("td","",new Date(info.time*1000).toLocaleString()));
-		this.node.appendChild(modules.node("td","",new Date(info.changed*1000).toLocaleString()));
-		this.node.appendChild(modules.node("td","",info.info));
+		this.node.appendChild(modules.node("td","element-status",info.status));
+		this.node.appendChild(modules.node("td","element-name",info.name));
+		this.node.appendChild(modules.node("td","element-address",info.address));
+		this.node.appendChild(modules.node("td","element-time",new Date(info.time*1000).toLocaleString()));
+		this.node.appendChild(modules.node("td","element-changed",new Date(info.changed*1000).toLocaleString()));
+		this.node.appendChild(modules.node("td","element-info",info.info));
 
 		document.getElementById("content-list").appendChild(this.node);
 
 		this.fetch = function(){
-			return;
+			var _self = this;
+			ajax.asyncGet("module/"+modules.modulename+"/hosts/"+_self.state.name+".json", function(request){
+				if(request.status != 200){
+					_self.node.getElementsByClassName("element-info")[0].textContent = "Failed to fetch";
+					return;
+				}
+				try{
+					var data = JSON.parse(request.responseText);
+				}
+				catch(e){
+					_self.node.getElementsByClassName("element-info")[0].textContent = "Failed to parse incoming data";
+					return;
+				}
+				_self.state = data;
+				_self.update();
+			},
+			function(e){
+				this.node.getElementsByClassName("element-info")[0].textContent = "Failed to fetch";
+			});
+		};
+
+		this.update = function(){
+			this.node.getElementsByClassName("element-info")[0].textContent = this.state.info;
+			this.node.getElementsByClassName("element-status")[0].textContent = this.state.status;
+			this.node.getElementsByClassName("element-time")[0].textContent = new Date(this.state.time*1000).toLocaleString();
+			this.node.getElementsByClassName("element-changed")[0].textContent = new Date(this.state.changed*1000).toLocaleString();
 		};
 	},
 
 	tosite:function(modname){
-		if ( modname == modules.modulename || modname == "" ){
+		if ( modname.module == modules.modulename || modname.module == "" ){
 			return;
 		}
-		modules.modulename = modname;
+		modules.modulename = modname.module;
 		modules.modulestates=[];
 		table = document.getElementById("content-list");
 		document.querySelectorAll('.content-element').forEach(function(ele){
@@ -58,8 +86,8 @@ var modules = {
 				window.alert("Failed to parse module json file: " + e);
 				return;
 			}
-			data.forEach(function(modname){
-				modules.modulestates.push(new modules.modulehost(modname));
+			data.forEach(function(moduleelement){
+				modules.modulestates.push(new modules.modulehost(moduleelement));
 			});
 			modules.updatemodule();
 		},
@@ -104,7 +132,7 @@ var modules = {
 			data.forEach(function(info){
 				modules.states.push(new modules.module(info));
 			});
-			modules.tosite(data[0].module);
+			modules.tosite(data[0]);
 		},
 		function(e){
 			window.alert("Failed to load status information: " + e);
